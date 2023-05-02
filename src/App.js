44d88f19907cf9d1d90de13a249bf10b85ecace5
LazyMan95 <nico95.nm@gmail.com>
2023-05-03 00:05:25 +0200

    Add test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-import logo from './logo.svg';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>color</p>
+        <br />
+      </div>
+      <div className="button">
+        <button>Generate</button>
+      </div>
     </div>
   );
 }
