f3aec1bcd68d684802940ba28382e05f271bae60
LazyMan95 <nico95.nm@gmail.com>
2023-05-03 15:03:14 +0200

    Add more changes

---
+++ README.old.md
@@ -0,0 +1,11 @@
+###TODO
+[x] - Create in html
+[x] - Generate button that will generate background color
+[x] - Color text with input what color is it
+[] - Create background color
+[] - text
+[] -
+[] -
+[] -
+[] -
+[] -
--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { randomizeBgColors } from 'background-color-randomizer';
+import randomColorRGB from 'random-color-rgb';
+import React, { useState } from 'react';
 
 export default function App() {
   return (
     <div className="App">
       <div>
-        <p>color</p>
+        <div>color:</div>
         <br />
-      </div>
-      <div className="button">
-        <button>Generate</button>
+        <button className="button"> Generate color</button>
+        <div className="input">
+          <br />
+          <div>hue:</div>
+          <input type="input" placeholder="Hex input" />
+          <div>Luminosity:</div>
+          <input type="input" placeholder="Hex input" />
+        </div>
       </div>
     </div>
   );
